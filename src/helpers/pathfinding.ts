4bd365e8ffba2269d85a821cd95a846c61c3dea2
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-12-17 18:02:00 +0100

    Fixed pathfinding

--- src/helpers/pathfinding.ts
+++ src/helpers/pathfinding.ts
@@ -1,34 +1,17 @@
 export default function(grid: string[][], start: { x: number; y: number }, end: { x: number; y: number }): { dist: number; path: number[][] } | null {
-	// console.log('searching for path')
-	// console.log(arguments)
 	if (start.x == end.x && start.y == end.y) return null
 
-	grid[start.x][start.y] = 'START'
-	grid[end.x][end.y] = 'END'
-
 	const queue: Array<{ coord: number[]; dist: number; path: number[][] }> = [{ coord: [start.x, start.y], dist: 0, path: [[start.x, start.y]] }]
-	const directs = [
-		[-1, 0],
-		[0, 1],
-		[1, 0],
-		[0, -1],
-	]
+	const directs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 	const isValidCoord = (x: number, y: number) => x >= 0 && x < grid.length && y >= 0 && y < grid.length
 
 	while (queue.length) {
-		let tmp = queue.shift()
-		// console.log(tmp)
-		if (tmp == undefined) throw new Error('this error will never throw, typescript is retarded')
-		const x: number = tmp.coord[0]
-		const y: number = tmp.coord[1]
-		const dist: number = tmp.dist
-		const path: number[][] = tmp.path
+		const { coord, dist, path } = queue.shift()!
+		const [x, y] = coord
 
 		// found end
 		if (x == end.x && y == end.y) {
-			let way = { dist, path }
-			// console.log('found:', way)
-			return way
+			return { dist, path }
 		}
 
 		// add move possible paths
@@ -36,13 +19,11 @@ export default function(grid: string[][], start: { x: number; y: number }, end: 
 			const nextX: number = x + moveX
 			const nextY: number = y + moveY
 
-			if (isValidCoord(nextX, nextY) && (grid[nextX][nextY] == 'empty' || grid[nextX][nextY] == 'END')) {
+			if (isValidCoord(nextX, nextY) && grid[nextX][nextY] == 'empty') {
 				queue.push({ coord: [nextX, nextY], dist: dist + 1, path: [...path, [nextX, nextY]] })
 				grid[nextX][nextY] = 'visited'
 			}
 		}
 	}
-
-	// console.log('not found')
 	return null
 }
