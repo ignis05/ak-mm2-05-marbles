20a286c4832d34b7827e45abe69369a7e6d7e7ce
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-12-17 18:07:31 +0100

    Update pathfinding

--- src/helpers/pathfinding.ts
+++ src/helpers/pathfinding.ts
@@ -2,20 +2,17 @@ export default function(grid: string[][], start: { x: number; y: number }, end: 
 	if (start.x == end.x && start.y == end.y) return null
 
 	const queue: Array<{ coord: number[]; dist: number; path: number[][] }> = [{ coord: [start.x, start.y], dist: 0, path: [[start.x, start.y]] }]
-	const directs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+	const directs = [[-1, 0],[0, 1],[1, 0],[0, -1]]
+
 	const isValidCoord = (x: number, y: number) => x >= 0 && x < grid.length && y >= 0 && y < grid.length
 
 	while (queue.length) {
 		const { coord, dist, path } = queue.shift()!
 		const [x, y] = coord
 
-		// found end
-		if (x == end.x && y == end.y) {
-			return { dist, path }
-		}
-
-		// add move possible paths
-		for (let [moveX, moveY] of directs) {
+		if (x == end.x && y == end.y) return { dist, path } // founbd end
+		
+		for (let [moveX, moveY] of directs) { // add valid cords to queue
 			const nextX: number = x + moveX
 			const nextY: number = y + moveY
 
